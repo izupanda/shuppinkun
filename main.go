786b2927037b40d2f332b0dca16c8ffd4825ad70
izupanda <izumi.handa@pandalab.jp>
2023-09-29 13:50:22 +0900

    chatGPT responce

--- main.go
+++ main.go
@@ -10,96 +10,98 @@ import (
 	"os"
 )
 
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
 type GenerateDescriptionRequest struct {
 	ProductName     string   `json:"productName"`
 	ProductWarnings []string `json:"productWarnings"`
 }
 
 type OpenAIRequestBody struct {
-	Prompt      string   `json:"prompt"`
-	MaxTokens   int      `json:"max_tokens,omitempty"`
-	Temperature float64  `json:"temperature,omitempty"`
-	Model       string   `json:"model,omitempty"`
-	Messages    []string `json:"messages,omitempty"`
+	MaxTokens   int       `json:"max_tokens,omitempty"`
+	Temperature float64   `json:"temperature,omitempty"`
+	Model       string    `json:"model,omitempty"`
+	Messages    []Message `json:"messages,omitempty"`
 }
 
 type OpenAIResponse struct {
 	Choices []struct {
-		Text string `json:"text"`
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
 	} `json:"choices"`
 }
 
 func handleGenerateDescription(w http.ResponseWriter, r *http.Request) {
-	// Decode the request body
+	log.Println("Received a request for generating product description.")
+
 	var requestBody GenerateDescriptionRequest
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		log.Println("Error:", err)
+		log.Println("Error in decoding the request body:", err)
 		return
 	}
+	log.Println("Successfully decoded the request body.")
 
 	openAIKey := os.Getenv("OPENAI_API_KEY")
 	if openAIKey == "" {
 		log.Fatal("OPENAI_API_KEY is not set")
 	}
 
-	// Create a request body for OpenAI API
+	messages := []Message{
+		{
+			Role:    "system",
+			Content: "You are a helpful assistant.",
+		},
+		{
+			Role:    "user",
+			Content: fmt.Sprintf("メルカリに出品するための商品説明文を生成してください。商品名は%sです。商品に関する注意事項として、%sを、商品説明に含めるようにしてください。", requestBody.ProductName, requestBody.ProductWarnings),
+		},
+	}
+
 	openAIRequest := OpenAIRequestBody{
-		Prompt:      fmt.Sprintf("フリマ向けの商品説明文を生成してください。商品名は%sで、商品に関する注意事項は、%sです。", requestBody.ProductName, requestBody.ProductWarnings),
-		MaxTokens:   10,
-		Temperature: 0.7,
-		Model:       "gpt-3.5-turbo",
-		Messages:    []string{},
+		MaxTokens:   500,
+		Temperature: 0.8,
+		Model:       "gpt-3.5-turbo-0613",
+		Messages:    messages,
 	}
 
 	requestBodyBytes, err := json.Marshal(openAIRequest)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
+		log.Println("Error in marshaling the OpenAI request body:", err)
 		return
 	}
+	log.Println("Successfully marshaled the OpenAI request body.")
+	log.Println("OpenAI API Request:", openAIRequest)
 
 	apiEndpoint := "https://api.openai.com/v1/chat/completions"
 	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(requestBodyBytes))
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
+		log.Println("Error in creating the OpenAI API request:", err)
 		return
 	}
 
-	// Set API Key and content type
 	req.Header.Set("Authorization", "Bearer "+openAIKey)
 	req.Header.Set("Content-Type", "application/json")
 
-	// Add Messages field to the request body
-	reqBody := make(map[string]interface{})
-	err = json.Unmarshal(requestBodyBytes, &reqBody)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
-		return
-	}
-	reqBody["messages"] = []string{}
-	requestBodyBytes, err = json.Marshal(reqBody)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
-		return
-	}
-
-	req.Body = ioutil.NopCloser(bytes.NewReader(requestBodyBytes))
-
 	client := &http.Client{}
+	log.Println("Sending request to OpenAI API...")
 	resp, err := client.Do(req)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
+		log.Println("Error in sending the request to OpenAI API:", err)
 		return
 	}
+	log.Println("Received response from OpenAI API.")
 	defer resp.Body.Close()
 
-	// Check for errors from OpenAI API
 	if resp.StatusCode >= 400 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		bodyString := string(bodyBytes)
@@ -112,13 +114,16 @@ func handleGenerateDescription(w http.ResponseWriter, r *http.Request) {
 	err = json.NewDecoder(resp.Body).Decode(&openAIResponse)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Println("Error:", err)
+		log.Println("Error in decoding the OpenAI API response:", err)
 		return
 	}
+	log.Println("Successfully decoded the OpenAI API response.")
+	log.Println("OpenAI API Response:", openAIResponse)
 
 	if len(openAIResponse.Choices) > 0 {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(openAIResponse.Choices[0].Text)
+		json.NewEncoder(w).Encode(openAIResponse.Choices[0].Message.Content)
+		log.Println("Generated product description:", openAIResponse.Choices[0].Message.Content)
 	} else {
 		http.Error(w, "OpenAI API returned empty choices", http.StatusInternalServerError)
 		log.Println("Error: OpenAI API returned empty choices")
@@ -127,11 +132,11 @@ func handleGenerateDescription(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("./static")) // `./static` は静的ファイルが格納されているディレクトリへのパス
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 
-	http.HandleFunc("/generate-description", handleGenerateDescription) // あなたの既存のエンドポイント
+	http.HandleFunc("/generate-description", handleGenerateDescription)
 
+	log.Println("Server is starting at :8080")
 	http.ListenAndServe(":8080", nil)
-
 }
